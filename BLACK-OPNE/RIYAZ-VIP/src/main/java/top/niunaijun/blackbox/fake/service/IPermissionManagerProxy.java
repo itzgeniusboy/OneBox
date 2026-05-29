81d1cf67c64c609f68ec8b74ec0251ed22d062c8
itzgeniusboy <107845877+itzgeniusboy@users.noreply.github.com>
2026-05-29 23:56:49 +0530

    Fix permission manager hook type mismatch

--- BLACK-OPNE/RIYAZ-VIP/src/main/java/top/niunaijun/blackbox/fake/service/IPermissionManagerProxy.java
+++ BLACK-OPNE/RIYAZ-VIP/src/main/java/top/niunaijun/blackbox/fake/service/IPermissionManagerProxy.java
@@ -1,7 +1,10 @@
 package top.niunaijun.blackbox.fake.service;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
-import android.os.Build;
+import android.util.Log;
+
+import java.lang.reflect.Field;
 
 import black.android.app.BRActivityThread;
 import black.android.app.BRContextImpl;
@@ -12,7 +15,6 @@ import top.niunaijun.blackbox.BlackBoxCore;
 import top.niunaijun.blackbox.fake.hook.BinderInvocationStub;
 import top.niunaijun.blackbox.fake.service.base.PkgMethodProxy;
 import top.niunaijun.blackbox.fake.service.base.ValueMethodProxy;
-import top.niunaijun.blackbox.utils.Reflector;
 import top.niunaijun.blackbox.utils.compat.BuildCompat;
 
 /**
@@ -35,17 +37,69 @@ public class IPermissionManagerProxy extends BinderInvocationStub {
 
     @Override
     protected void inject(Object baseInvocation, Object proxyInvocation) {
-        replaceSystemService("permissionmgr");
-        BRActivityThread.getWithException()._set_sPermissionManager(proxyInvocation);
+        replaceSystemService(P);
+        try {
+            BRActivityThread.getWithException()._set_sPermissionManager(proxyInvocation);
+        } catch (Throwable e) {
+            Log.w(TAG, "Unable to replace ActivityThread.sPermissionManager", e);
+        }
+
         Object systemContext = BRActivityThread.get(BlackBoxCore.mainThread()).getSystemContext();
         PackageManager packageManager = BRContextImpl.get(systemContext).mPackageManager();
         if (packageManager != null) {
+            installApplicationPackagePermissionManager(systemContext, packageManager, proxyInvocation);
+        }
+    }
+
+    private void installApplicationPackagePermissionManager(Object systemContext, PackageManager packageManager, Object proxyInvocation) {
+        try {
+            Field permissionManagerField = findField(Class.forName("android.app.ApplicationPackageManager"), "mPermissionManager");
+            permissionManagerField.setAccessible(true);
+            Class<?> fieldType = permissionManagerField.getType();
+            if (proxyInvocation != null && fieldType.isInstance(proxyInvocation)) {
+                permissionManagerField.set(packageManager, proxyInvocation);
+                Log.d(TAG, "Installed permission binder proxy into ApplicationPackageManager.mPermissionManager");
+                return;
+            }
+
+            Object typedPermissionManager = getTypedPermissionManager(systemContext, permissionManagerField, packageManager, fieldType);
+            if (typedPermissionManager != null && fieldType.isInstance(typedPermissionManager)) {
+                permissionManagerField.set(packageManager, typedPermissionManager);
+                Log.d(TAG, "Kept typed PermissionManager wrapper for ApplicationPackageManager.mPermissionManager");
+            } else {
+                String proxyType = proxyInvocation == null ? "null" : proxyInvocation.getClass().getName();
+                Log.w(TAG, "Skipping ApplicationPackageManager.mPermissionManager hook because field type "
+                        + fieldType.getName() + " is not compatible with " + proxyType);
+            }
+        } catch (Throwable e) {
+            Log.w(TAG, "Unable to install ApplicationPackageManager permission hook", e);
+        }
+    }
+
+
+    private Field findField(Class<?> type, String name) throws NoSuchFieldException {
+        for (Class<?> clazz = type; clazz != null; clazz = clazz.getSuperclass()) {
             try {
-                Reflector.on("android.app.ApplicationPackageManager").field("mPermissionManager").set(packageManager, proxyInvocation);
-            } catch (Exception e) {
-                e.printStackTrace();
+                return clazz.getDeclaredField(name);
+            } catch (NoSuchFieldException ignored) {
+                // Continue walking the hierarchy.
             }
         }
+        throw new NoSuchFieldException(name);
+    }
+
+    private Object getTypedPermissionManager(Object systemContext, Field permissionManagerField,
+                                             PackageManager packageManager, Class<?> fieldType)
+            throws IllegalAccessException {
+        Object currentPermissionManager = permissionManagerField.get(packageManager);
+        if (currentPermissionManager != null && fieldType.isInstance(currentPermissionManager)) {
+            return currentPermissionManager;
+        }
+        if ("android.permission.PermissionManager".equals(fieldType.getName())
+                && systemContext instanceof Context) {
+            return ((Context) systemContext).getSystemService(fieldType);
+        }
+        return null;
     }
 
     @Override
